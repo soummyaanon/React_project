899b41403ef476209492d6accdcebb23c0112120
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-26 11:53:40 +0530

    Add React and Vite project files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,6 @@
+function App() {
+  return (
+    <h1>Hello, world!</h1>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello World with vite | srp</h1>
+  );
+}
+
+export default App;
