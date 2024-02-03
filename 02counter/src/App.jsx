c27931ee784e531edfd385ec11e078732ebf11dc
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-02-04 00:16:40 +0530

    My first react project A Counter Application I have Learned how can i Used hooks in React

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Remove unused imports
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    if (counter<30){
+      setCounter(counter + 1);
+    }
+  };
+
+  const removevalue = () => {
+    if (counter>0){
+      setCounter(counter - 1);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My First React Project</h1>
+      <h2> Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value{counter} </button>
+      <br />
+      <button onClick={removevalue}>Subtract Value {counter}</button>
+      <br/>
+      <footer> Foooter {counter}</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
