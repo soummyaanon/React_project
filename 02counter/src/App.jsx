e1031d613df56745f7b6c067e9acfdf504338b46
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-02-05 23:34:59 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -16,13 +16,20 @@ function App() {
     }
   }
 
+  const resetCounter = () => {
+    setCounter(5);
+  }
+
   return (
     <div className="App">
       <h1>My First React Project</h1>
       <h2> Counter Value: {counter}</h2>
       <button onClick={addValue}>Add Value</button>
       <button onClick={removevalue}>Subtract Value</button>
-      <footer> Footer {counter}</footer>
+      <footer> 
+        Footer {counter}
+        <button onClick={resetCounter}>Reset Counter</button>
+      </footer>
     </div>
   );
 }
