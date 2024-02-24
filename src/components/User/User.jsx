f0f472dd9290cc7eef1740e63ab3b82c7389ade6
soummyaanon <92160127+yimkumer@users.noreply.github.com>
2024-02-25 02:35:48 +0530

    React Router Learnings

---
+++ src/components/User/User.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {userid} = useParams()
+  return (
+    <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
+  )
+}
+
+export default User
