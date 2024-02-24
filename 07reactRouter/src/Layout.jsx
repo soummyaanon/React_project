a0319cd93bf26f7d1c22410ecaf725650638b8c9
soummyaanon <92160127+yimkumer@users.noreply.github.com>
2024-02-25 02:43:44 +0530

    Router project

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
