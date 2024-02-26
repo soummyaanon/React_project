93124842acef15be9452d7913731a0cb51ebc6bb
soummyaanon <92160127+yimkumer@users.noreply.github.com>
2024-02-26 11:31:38 +0530

    github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/soummyaanon"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
